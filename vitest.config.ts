be19665028d39fa9ce369c3ded92c605c5137d81
Wilmer Garzon <wilmerj1996@gmail.com>
2024-10-27 17:21:50 -0500

    feat: config coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,5 +5,17 @@ export default defineConfig({
     globals: true,
     environment: "jsdom",
     setupFiles: "./src/setupTests.ts",
+    coverage: {
+      exclude: [
+        "**/*.config.ts",
+        "**/*.config.js",
+        "**/*.type.ts",
+        "**/*.d.ts",
+        "**/types",
+        "dist/assets",
+        "**/App.tsx",
+        "**/main.tsx",
+      ],
+    },
   },
 });
