08030f36a14853b8de1e0053aee3a44f4cfbd697
Wilmer Garzon <wilmerj1996@gmail.com>
2024-10-27 17:06:36 -0500

    feat: add two test cases

--- src/hooks/useOrdersMsw.test.tsx
+++ src/hooks/useOrdersMsw.test.tsx
@@ -1,8 +1,10 @@
 import { renderHook } from "@testing-library/react-hooks";
 import { MemoryRouter } from "react-router-dom";
 import { describe, it, expect, Mock, vi, beforeEach } from "vitest";
-import { useOrders } from "./useOrders";
 import { SessionProvider, useSession } from "../context/AuthContext";
+import { http, HttpResponse } from "msw";
+import { server } from "../mocks/server";
+import { useOrders } from "./useOrders";
 import { waitFor } from "@testing-library/dom";
 
 // Mock del contexto de autenticación
@@ -17,7 +19,7 @@ vi.mock("../context/AuthContext", async () => {
 describe("useOrders hook", () => {
   // Configuración común para todos los tests
   const mockUser = { id: "1", name: "Test User" };
-  
+
   beforeEach(() => {
     (useSession as Mock).mockReturnValue({ user: mockUser });
   });
@@ -43,8 +45,25 @@ describe("useOrders hook", () => {
     expect(result.current.error).toBeNull();
   });
 
-  it("should show loading state while fetching", () => {
+  it("should failed fetch orders", async () => {
+    server.use(
+      http.get("http://localhost:3001/orders", () => {
+        return new HttpResponse(null, {
+          status: 500,
+          statusText: "Internal Server Error",
+        });
+      })
+    );
+
     const { result } = renderHook(() => useOrders(), { wrapper });
-    expect(result.current.loading).toBe(true);
+
+    await waitFor(() => {
+      expect(result.current.loading).toBe(false);
+    });
+
+    expect(result.current.error).toBe(
+      "Failed to fetch orders. Please try again later."
+    );
+    expect(result.current.orders).toEqual([]);
   });
 });
